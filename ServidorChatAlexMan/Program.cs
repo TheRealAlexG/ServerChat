34a852b3a15c924e0947229667e8901787b5df70
alexa <alexa@ALEXTHETOPG>
2024-02-11 19:57:15 +0100

    Add project files.

---
+++ ServidorChatAlexMan/Program.cs
@@ -0,0 +1,109 @@
+﻿using ServidorChatAlexMan;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    const int HTTP_TOT_OK = 200;
+    const int HTTP_ERROR = 500;
+    const string URL_SERVIDOR = "http://localhost:51111/";
+
+    private static userChat chatServer;
+
+    static async Task Main(string[] args)
+    {
+        chatServer = new userChat();
+        await ListenAsync(chatServer);
+        Console.ReadLine();
+    }
+
+    private static async Task ListenAsync(userChat chatServer)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add(URL_SERVIDOR);
+        listener.Start();
+
+        try
+        {
+            while (true)
+            {
+                HttpListenerContext context = null;
+
+                try
+                {
+                    context = await listener.GetContextAsync();
+                    string url = context.Request.Url.Segments.LastOrDefault()?.Trim('/').ToLower();
+
+                    if (url.StartsWith("registrarusuari"))
+                    {
+                        string usuari = context.Request.QueryString.Get("u");
+                        chatServer.Users.Add(usuari);
+                        await EscriureRespostaAsync(context, usuari, HTTP_TOT_OK);
+                    }
+                    else if (url.StartsWith("enviar"))
+                    {
+                        string usuariDesti = context.Request.QueryString.Get("u");
+                        string usuariEmissor = context.Request.QueryString.Get("ue");
+                        string missatge = context.Request.QueryString.Get("m");
+
+                        chatServer.saveMessage(usuariDesti, usuariEmissor, missatge);
+                        await EscriureRespostaAsync(context, missatge, HTTP_TOT_OK);
+                    }
+                    else if (url.StartsWith("rebre"))
+                    {
+                        string usuari = context.Request.QueryString.Get("u");
+                        var missatgesRecents = chatServer.reciveMessage(usuari);
+                        string resposta = string.Join("; ", missatgesRecents);
+                        await EscriureRespostaAsync(context, resposta, HTTP_TOT_OK);
+                    }
+                    else if (url.StartsWith("rperdatahora"))
+                    {
+                        string usuari = context.Request.QueryString.Get("u");
+                        string dataHoraString = context.Request.QueryString.Get("t");
+
+                        if (DateTime.TryParseExact(dataHoraString, "dd/MM/yyyy H", null, System.Globalization.DateTimeStyles.None, out DateTime dataHora))
+                        {
+                            var llistaMissatges = chatServer.reciveMessageTime(usuari, dataHora);
+                            string resposta = string.Join("; ", llistaMissatges);
+                            await EscriureRespostaAsync(context, resposta, HTTP_TOT_OK);
+                        }
+                        else
+                        {
+                            await EscriureRespostaAsync(context, "Format de data/hora incorrecte", HTTP_ERROR);
+                        }
+                    }
+                    else if (url.StartsWith("llistarusuaris"))
+                    {
+                        var llistaUsuaris = chatServer.usersList();
+                        string resposta = string.Join("; ", llistaUsuaris);
+                        await EscriureRespostaAsync(context, resposta, HTTP_TOT_OK);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processant la solicitud: {ex.Message}");
+                    Console.WriteLine(ex.StackTrace);
+                    await EscriureRespostaAsync(context, $"Error intern del servidor: {ex.Message}", HTTP_ERROR);
+                }
+                finally
+                {
+                    if (context != null) context.Response.Close();
+                }
+            }
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+
+    private static async Task EscriureRespostaAsync(HttpListenerContext context, string msg, int codiError)
+    {
+        context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+        using (Stream s = context.Response.OutputStream)
+        using (StreamWriter writer = new StreamWriter(s))
+            await writer.WriteAsync(msg);
+    }
+
+}
---
+++ ServidorChatAlexMan/userChat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorChatAlexMan
+{
+    public class userChat
+    {
+
+        private Dictionary<string, List<string>> userMessages;
+        private List<string> users;
+
+        public List<string> Users { get => users; set => users = value; }
+
+        public userChat()
+        {
+            userMessages = new Dictionary<string, List<string>>();
+            this.users = new List<string>();
+        }
+
+        public void saveMessage(string destinationUser, string sendUser, string message)
+        {
+            if (!userMessages.ContainsKey(destinationUser)) userMessages[destinationUser] = new List<string>();
+
+            if (!userMessages.ContainsKey(sendUser)) userMessages[sendUser] = new List<string>();
+
+            userMessages[sendUser].Add($"Enviat: a {destinationUser} ({DateTime.Now}): {message}");
+            userMessages[destinationUser].Add($"Rebut: de {sendUser} ({DateTime.Now}): {message}");
+        }
+
+        public List<string> reciveMessage(string nomUsuari)
+        {
+            // Retorna els missatges de l'usuari o una llista buida si l'usuari no existeix
+            return userMessages.ContainsKey(nomUsuari) ? userMessages[nomUsuari] : new List<string>();
+        }
+
+        public List<string> reciveMessageTime(string nomUsuari, DateTime dataIHora)
+        {
+            // Retorna els missatges posteriors a la data/hora proporcionada
+            List<string> missatgesPosteriors = new List<string>();
+
+            if (userMessages.ContainsKey(nomUsuari))
+            {
+                foreach (var missatge in userMessages[nomUsuari])
+                {
+                    // Obté la data/hora del missatge (suposant que el format és correcte)
+
+                    var parts = missatge.Split(' ');
+                    var data = parts[1].Trim('(');
+                    var hora = parts[2].Split(':');
+
+                    StringBuilder sb = new StringBuilder(data);
+                    sb.Append(' ');
+                    sb.Append(hora[0]);
+
+                    if (DateTime.TryParseExact(sb.ToString(), "dd/MM/yyyy H", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dataMissatge))
+                    {
+                        if (dataMissatge == dataIHora) missatgesPosteriors.Add(missatge);
+                    }
+                }
+            }
+
+            return missatgesPosteriors;
+        }
+
+        public List<string> usersList()
+        {
+            return this.Users;
+        }
+
+
+    }
+}
